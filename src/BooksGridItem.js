c10517c820133b69f86f20da7098a2a08fe0e21a
rogeralbinoi <roger.albino.i@gmail.com>
2017-08-22 23:46:01 -0300

    refactor:  Add  books grid item component

---
+++ src/BooksGridItem.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+
+class BooksGridItem extends Component {
+    state = {
+        shelf: this.props.book.shelf || 'none'
+    }
+
+    handleChangeShelf = (shelf) => {
+        this.setState({shelf})
+    }
+
+    render() {
+        let {book} = this.props
+        return (
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.smallThumbnail}")` }}>
+                        </div>
+                        <div className="book-shelf-changer">
+                        <select value={this.state.shelf} onChange={(e) => {this.handleChangeShelf(e.target.value)}}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                        </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">
+                        {
+                            book.authors && book.authors.map((author, key) => {
+                                if(key > 0) {
+                                    return ` & ${author}`
+                                }else {
+                                    return author
+                                }
+                            })
+                        }
+                    </div>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default BooksGridItem
